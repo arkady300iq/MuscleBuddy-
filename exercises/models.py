421e254ae6f520471c674cd04d2b2930e3064643
arkady300iq <arkadijrivior@gmail.com>
2024-12-07 13:39:02 +0200

    video for exercises

--- exercises/models.py
+++ exercises/models.py
@@ -4,6 +4,7 @@ class Exercise(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
     photo = models.ImageField(upload_to="training_days", blank=True, null=True)
+    video_file = models.FileField(upload_to="exercise_videos/", blank=True, null=True)
 
     def __str__(self):
         return self.name
