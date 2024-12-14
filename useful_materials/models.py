33a496a983100cdfb9cd3a4a789dc27879480864
arkady300iq <arkadijrivior@gmail.com>
2024-12-14 14:55:41 +0200

    navbar changed

--- useful_materials/models.py
+++ useful_materials/models.py
@@ -5,6 +5,7 @@ class Useful_Materials(models.Model):
     description = models.TextField(null=True, blank=True)
     video = models.FileField(upload_to="meterials_video", blank=True, null=True)
     image = models.FileField(upload_to="materials_photo", blank=True, null=True)
+    app_link_name =models.CharField(max_length=100, blank=True, null=True)
     app_link = models.URLField(blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
 
