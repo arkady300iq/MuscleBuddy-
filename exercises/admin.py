0852d636c563f49017a9a51c1e8b954b943b311f
arkady300iq <arkadijrivior@gmail.com>
2024-12-25 00:51:38 +0200

    some changes

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 from exercises.models import Exercise
 admin.site.register(Exercise)
+
--- exercises/models.py
+++ exercises/models.py
@@ -3,8 +3,13 @@ from django.db import models
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    description_set = models.TextField(null=True, blank=True)
     photo = models.ImageField(upload_to="training_days/", blank=True, null=True)
     video_file = models.FileField(upload_to="exercise_videos/", blank=True, null=True)
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']  
 
     def __str__(self):
         return self.name
